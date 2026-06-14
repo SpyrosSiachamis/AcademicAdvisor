93eaaf4d739e4a5a6d74f9721f0703c5e4a469c6
SpyrosSiachamis <csd5503@csd.uoc.gr>
2026-06-14 21:38:26 +0300

    Add user module with schema and router files

---
+++ backend/src/modules/users/schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Annotated
+# NOTE: UserCreate includes id and plaintext password only for the in-memory alpha.
+# PostgreSQL/auth will replace this with generated IDs and password hashes.
+
+class UserCreate(BaseModel):
+    id: Annotated[int, Field(ge=1)] #TODO remove when Database replaces in-memory storage
+    username: Annotated[str, Field(min_length=3, max_length=30)]
+    email: EmailStr
+    password: Annotated[str, Field(min_length=5)] #TODO Temporary password field, will need to be hashed
+    department_id: Annotated[int, Field(ge=1)]
+
+class UserRead(BaseModel):
+    id: Annotated[int, Field(ge=1)]
+    username: Annotated[str, Field(min_length=3, max_length=30)]
+    email: EmailStr
+    department_id: Annotated[int, Field(ge=1)]
