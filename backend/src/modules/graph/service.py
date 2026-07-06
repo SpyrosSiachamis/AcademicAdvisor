c9fc40067d958e83550aeb7d77fdf55b830419fd
Spiros Siahamis <siahamissp@gmail.com>
2026-07-06 20:05:25 +0300

    add course eligibility retrieval function and import courses

--- backend/src/modules/graph/service.py
+++ backend/src/modules/graph/service.py
@@ -1,6 +1,7 @@
-from ..storage.memory import course_attempts
+from ..storage.memory import course_attempts, courses
 from ..course_attempt.schema import Status
 from ..graph.builder import build_department_prerequisite_adj_list
+from typing import Any
 def get_passed_courses(user_id: int) -> set[int]:
     passed_courses: set[int] = set()
     for attempt in course_attempts:
@@ -11,3 +12,13 @@ def get_passed_courses(user_id: int) -> set[int]:
 def get_course_prerequisites(course_id: int) -> list[list[int]]:
     course_prerequisites: dict[int, list[list[int]]] = build_department_prerequisite_adj_list()
     return course_prerequisites[course_id]
+
+def get_all_course_eligibilities(user_id: int) -> dict[int,bool]:
+    from ..eligibility.router import get_course_eligibility
+    eligibilities: dict[int, bool] = {}
+
+    for course in courses:
+        course_id: int = course['id']
+        result = get_course_eligibility(user_id,course_id)
+        eligibilities[course_id] = result["eligible"]
+    return eligibilities
